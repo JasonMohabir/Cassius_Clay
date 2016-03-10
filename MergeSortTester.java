1ee3a860de26613e1d037dae242851ad1d7710fd
ShaAli <sali1@stuy.edu>
2016-03-09 23:19:51 -0500

    testing almost done

--- MergeSortTester.java
+++ MergeSortTester.java
@@ -30,17 +30,19 @@ public class MergeSortTester
       int[] mill = new int[1000000];*/
     
     //populating the arrays with random numbers
-    for(int i : ten){
-	i = (int)Math.random()*100;
-    }
-    for(int i : hun){
-	i = (int)Math.random()*1000;
-    }
-    for(int i : thou){
-	i = (int)Math.random()*10000;
-    }
-    for(int i : tenthou){
-	i = (int)Math.random()*100000;
+    public void initArrays(){
+	for(int i : ten){
+	    i = (int)Math.random()*100;
+	}
+	for(int i : hun){
+	    i = (int)Math.random()*1000;
+	}
+	for(int i : thou){
+	    i = (int)Math.random()*10000;
+	}
+	for(int i : tenthou){
+	    i = (int)Math.random()*100000;
+	}
     }
     
     
@@ -53,7 +55,35 @@ public class MergeSortTester
      ******************************/
     public static void main( String[] args ) 
     {
+	MergeSortTester Test = new MergeSortTester();
+	Test.initArrays();
 
+	long start = System.nanoTime();
+	MergeSort.sort(Test.ten);
+	long end = System.nanoTime();
+	long runtime1 = end - start;
+
+	start = System.nanoTime();
+	MergeSort.sort(Test.hun);
+	end = System.nanoTime();
+	long runtime2 = end - start;
+
+	start = System.nanoTime();
+	MergeSort.sort(Test.thou);
+	end = System.nanoTime();
+	long runtime3 = end - start;
+
+	start = System.nanoTime();
+	MergeSort.sort(Test.tenthou);
+	end = System.nanoTime();
+	long runtime4 = end - start;
+
+	System.out.println("Runtimes:");
+	System.out.println("10 element array: "+runtime1+"nanoseconds");
+	System.out.println("100 element array: "+runtime2+"nanoseconds");
+	System.out.println("1000 element array: "+runtime3+"nanoseconds");
+	System.out.println("10000 element array: "+runtime4+"nanoseconds");
+		   
     }//end main
 
 }//end class
